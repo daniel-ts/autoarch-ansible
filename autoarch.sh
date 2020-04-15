586d7b9b177a5145664f48b58a7e246cceb6b5e1
dandy <daniel.tschertkow@posteo.de>
2020-04-15 15:18:21 +0200

    modified:   MountUnlock.yml removed vault in tmpdir
    modified:   autoarch.sh
    variable was lacking a $

--- autoarch.sh
+++ autoarch.sh
@@ -17,7 +17,7 @@ alias SYS_CHROOT="arch-chroot $SYS_MOUNT"
 AP_PARAMS='-vv --accept-host-key -i inventory --url "https://github.com/daniel-ts/autoarch-ansible.git"'
 
 if hostname "$1"; then
-    ansible-pull AP_PARAMS AutoarchBase.yml \
+    ansible-pull $AP_PARAMS AutoarchBase.yml \
 	&& SYS_CHROOT ansible-pull $AP_PARAMS AutoarchSystemConfig.yml \
 	&& SYS_CHROOT ansible-pull $AP_PARAMS AutoarchSystemConfig.yml
 fi
